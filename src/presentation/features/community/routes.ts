24f2a2b375958bfe31a291c6582d66545403ffa9
Juan Gonon <juanfegonon27@gmail.com>
2025-08-13 12:03:47 -0600

    validate update and delete express validator

--- src/presentation/features/community/routes.ts
+++ src/presentation/features/community/routes.ts
@@ -2,7 +2,7 @@
 import { Router } from 'express'
 import { CommunityController } from './controller'
 import { CommunityService } from '../../services/Community.service'
-import { param } from 'express-validator'
+import { param, body } from 'express-validator'
 import { PARAMS_BODY } from '../../../constants/params.c'
 import { validationMessages } from '../../../constants/validationMessage.c'
 import { ValidateFields } from '../../middleware/ValidateFields'
@@ -15,9 +15,58 @@ export class CommunityRoutes {
 
     router.get('/', controller.getAllCommunities)
     router.get('/:id', [param(PARAMS_BODY.id).isInt().withMessage(validationMessages.notInteger('Id_comunidad')), ValidateFields.validate], controller.getCommunityById)
-    router.post('/', controller.createCommunity)
-    router.put('/:id', controller.updateCommunity)
-    router.delete('/:id', controller.deleteCommunity)
+    router.post('/',
+      [
+        body(PARAMS_BODY.nombre)
+          .notEmpty().withMessage(validationMessages.required(PARAMS_BODY.nombre)),
+
+        body(PARAMS_BODY.direccion)
+          .notEmpty().withMessage(validationMessages.required(PARAMS_BODY.direccion)),
+
+        body('id_parroquia')
+          .notEmpty().withMessage(validationMessages.required('id_parroquia'))
+          .isInt().withMessage(validationMessages.notInteger('id_parroquia')),
+
+        body(PARAMS_BODY.telefono)
+          .optional()
+          .matches(/^\d{8}$/).withMessage('The phone number must have exactly 8 digits'),
+
+        body(PARAMS_BODY.email)
+          .optional()
+          .isEmail().withMessage(validationMessages.invalidEmail(PARAMS_BODY.email)),
+
+        ValidateFields.validate
+      ],
+      controller.createCommunity)
+    router.put('/:id',
+      [
+        param(PARAMS_BODY.id)
+          .isInt().withMessage(validationMessages.notInteger(PARAMS_BODY.id)),
+
+        body(PARAMS_BODY.nombre)
+          .optional()
+          .notEmpty().withMessage(validationMessages.required(PARAMS_BODY.nombre)),
+
+        body(PARAMS_BODY.direccion)
+          .optional()
+          .notEmpty().withMessage(validationMessages.required(PARAMS_BODY.direccion)),
+
+        body('id_parroquia')
+          .optional()
+          .isInt().withMessage(validationMessages.notInteger('id_parroquia')),
+
+        body(PARAMS_BODY.telefono)
+          .optional()
+          .matches(/^\d{8}$/).withMessage('The phone number must have exactly 8 digits'),
+
+        body(PARAMS_BODY.email)
+          .optional()
+          .isEmail().withMessage(validationMessages.invalidEmail(PARAMS_BODY.email)),
+
+        ValidateFields.validate
+      ],
+      controller.updateCommunity)
+    router.delete('/:id', [param(PARAMS_BODY.id).isInt().withMessage(validationMessages.notInteger('Id_comunidad')), ValidateFields.validate], controller.deleteCommunity)
 
     return router
   }
