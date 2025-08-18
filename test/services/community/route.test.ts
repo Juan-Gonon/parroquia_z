cde0e67903f2d2d6d7a3cc28267e12b47af2764a
Juan Gonon <juanfegonon27@gmail.com>
2025-08-17 23:08:08 -0600

    test create new Community and getById

--- test/services/community/route.test.ts
+++ test/services/community/route.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 // import { prisma } from '../../../src/data/postgress'
 import request from 'supertest'
 import { testServer } from '../../presentation/test-server'
@@ -11,11 +12,53 @@ describe('Community route testing', () => {
     testServer.close()
   })
 
+  const community = {
+    nombre: 'Rosario',
+    direccion: 'Colonia el rosario',
+    email: 'rosario@gmail.com',
+    telefono: '12345678',
+    id_parroquia: 1
+  }
+  let id
+
   it('should return a Community api/community/', async () => {
     const { body } = await request(testServer.app)
       .get('/api/communities')
       .expect(200)
 
     expect(body.comunities).toBeInstanceOf(Array)
+  })
+
+  it('should create a new community when posting valid data to /api/communities', async () => {
+    const { body } = await request(testServer.app)
+      .post('/api/communities')
+      .send(community)
+      .expect(200)
+
+    id = body.id_comunidad
+
+    expect(body).toEqual({
+      id_comunidad: expect.any(Number),
+      nombre: community.nombre,
+      direccion: community.direccion,
+      telefono: community.telefono,
+      email: community.email,
+      id_parroquia: community.id_parroquia
+    })
+  })
+
+  it('should return a community when requesting /api/communities/:id', async () => {
+    const { body } = await request(testServer.app)
+      .get(`/api/communities/${id}`)
+      .expect(200)
+
+    expect(body).toEqual({
+      id_comunidad: expect.any(Number),
+      nombre: community.nombre,
+      direccion: community.direccion,
+      telefono: community.telefono,
+      email: community.email,
+      id_parroquia: community.id_parroquia
+    })
   })
 })
