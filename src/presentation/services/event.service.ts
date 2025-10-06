7943c1a430200b0595580cf0b07325be2912986b
Juan Gonon <juanfegonon27@gmail.com>
2025-10-06 10:01:14 -0600

    get all events front

--- src/presentation/services/event.service.ts
+++ src/presentation/services/event.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable @typescript-eslint/space-before-function-paren */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
@@ -9,9 +11,9 @@ import { UpdateEventoDto } from '../../domain/DTOs/event/UpdateEvent.dto'
 
 export class EventoService {
   // Obtener un registro de evento por su ID
-  public async getEventById (id: number): Promise<any> {
+  public async getEventById(id: number): Promise<any> {
     const event = await prisma.evento.findUnique({
-      where: { id_evento: id }
+      where: { id_evento: id },
     })
     if (!event) {
       throw CustomError.badRequest('The requested event was not found.')
@@ -20,49 +22,57 @@ export class EventoService {
   }
 
   // Obtener todos los eventos con paginación
-  public async getAllEvents (paginationDto: PaginationDto): Promise<any> {
+  public async getAllEvents(paginationDto: PaginationDto): Promise<any> {
     const { page, limit } = paginationDto
 
     try {
-      const [total, events] = await prisma.$transaction([
-        prisma.evento.count(),
-        prisma.evento.findMany({
-          skip: (page - 1) * limit,
-          take: limit
-        })
-      ])
+      // const [total, events] = await prisma.$transaction([
+      //   prisma.evento.count(),
+      //   prisma.evento.findMany({
+      //     skip: (page - 1) * limit,
+      //     take: limit
+      //   })
+      // ])
+
+      const events = await prisma.evento.findMany()
 
       return {
         page,
         limit,
-        total,
-        next: (page * limit) < total ? `/api/event?page=${page + 1}&limit=${limit}` : null,
-        prev: (page - 1 > 0) ? `/api/event?page=${page - 1}&limit=${limit}` : null,
-        data: events
+        // total,
+        // next: (page * limit) < total ? `/api/event?page=${page + 1}&limit=${limit}` : null,
+        // prev: (page - 1 > 0) ? `/api/event?page=${page - 1}&limit=${limit}` : null,
+        data: events,
       }
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while retrieving the data.')
+      throw CustomError.internalServer(
+        'An error occurred while retrieving the data.'
+      )
     }
   }
 
   // Crear un nuevo evento
-  public async createEvent (
-    createEventoDto: CreateEventoDto
-  ): Promise<any> {
+  public async createEvent(createEventoDto: CreateEventoDto): Promise<any> {
     // Verificar si las relaciones (Comunidad, TipoEvento) existen
-    const comunidadExists = await prisma.comunidad.findUnique({ where: { id_comunidad: createEventoDto.idComunidad } })
+    const comunidadExists = await prisma.comunidad.findUnique({
+      where: { id_comunidad: createEventoDto.idComunidad },
+    })
     if (!comunidadExists) {
       throw CustomError.badRequest('The specified community does not exist.')
     }
 
-    const tipoEventoExists = await prisma.tipoevento.findUnique({ where: { id_tipo: createEventoDto.idTipoEvento } })
+    const tipoEventoExists = await prisma.tipoevento.findUnique({
+      where: { id_tipo: createEventoDto.idTipoEvento },
+    })
     if (!tipoEventoExists) {
       throw CustomError.badRequest('The specified event type does not exist.')
     }
 
     // Validar si el celebrante existe si se proporciona un id_celebrante
     if (createEventoDto.idCelebrante) {
-      const celebranteExists = await prisma.personalparroquial.findUnique({ where: { id_personal: createEventoDto.idCelebrante } })
+      const celebranteExists = await prisma.personalparroquial.findUnique({
+        where: { id_personal: createEventoDto.idCelebrante },
+      })
       if (!celebranteExists) {
         throw CustomError.badRequest('The specified celebrant does not exist.')
       }
@@ -80,41 +90,50 @@ export class EventoService {
           aceptaintenciones: createEventoDto.aceptaIntenciones ?? false,
           requiereinscripcion: createEventoDto.requiereInscripcion ?? false,
           id_celebrante: createEventoDto.idCelebrante ?? null,
-          nombrecelebranteexterno: createEventoDto.nombreCelebranteExterno ?? null
-        }
+          nombrecelebranteexterno:
+            createEventoDto.nombreCelebranteExterno ?? null,
+        },
       })
       return newEvent
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while creating the event.')
+      throw CustomError.internalServer(
+        'An error occurred while creating the event.'
+      )
     }
   }
 
   // Actualizar un evento
-  public async updateEvent (
-    updateEventoDto: UpdateEventoDto
-  ): Promise<any> {
+  public async updateEvent(updateEventoDto: UpdateEventoDto): Promise<any> {
     const eventExists = await this.getEventById(updateEventoDto.id)
     if (!eventExists) {
-      throw CustomError.badRequest('The requested event to update was not found.')
+      throw CustomError.badRequest(
+        'The requested event to update was not found.'
+      )
     }
 
     // Validar las relaciones si se están actualizando
     if (updateEventoDto.idComunidad) {
-      const comunidadExists = await prisma.comunidad.findUnique({ where: { id_comunidad: updateEventoDto.idComunidad } })
+      const comunidadExists = await prisma.comunidad.findUnique({
+        where: { id_comunidad: updateEventoDto.idComunidad },
+      })
       if (!comunidadExists) {
         throw CustomError.badRequest('The specified community does not exist.')
       }
     }
 
     if (updateEventoDto.idTipoEvento) {
-      const tipoEventoExists = await prisma.tipoevento.findUnique({ where: { id_tipo: updateEventoDto.idTipoEvento } })
+      const tipoEventoExists = await prisma.tipoevento.findUnique({
+        where: { id_tipo: updateEventoDto.idTipoEvento },
+      })
       if (!tipoEventoExists) {
         throw CustomError.badRequest('The specified event type does not exist.')
       }
     }
 
     if (updateEventoDto.idCelebrante) {
-      const celebranteExists = await prisma.personalparroquial.findUnique({ where: { id_personal: updateEventoDto.idCelebrante } })
+      const celebranteExists = await prisma.personalparroquial.findUnique({
+        where: { id_personal: updateEventoDto.idCelebrante },
+      })
       if (!celebranteExists) {
         throw CustomError.badRequest('The specified celebrant does not exist.')
       }
@@ -123,27 +142,33 @@ export class EventoService {
     try {
       const updatedEvent = await prisma.evento.update({
         where: { id_evento: updateEventoDto.id },
-        data: updateEventoDto.values
+        data: updateEventoDto.values,
       })
       return updatedEvent
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while updating the event.')
+      throw CustomError.internalServer(
+        'An error occurred while updating the event.'
+      )
     }
   }
 
   // Eliminar un evento
-  public async deleteEvent (id: number): Promise<any> {
+  public async deleteEvent(id: number): Promise<any> {
     const eventExists = await this.getEventById(id)
     if (!eventExists) {
-      throw CustomError.badRequest('The requested event to delete was not found.')
+      throw CustomError.badRequest(
+        'The requested event to delete was not found.'
+      )
     }
     try {
       await prisma.evento.delete({
-        where: { id_evento: id }
+        where: { id_evento: id },
       })
       return { message: 'Event successfully deleted' }
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while deleting the event.')
+      throw CustomError.internalServer(
+        'An error occurred while deleting the event.'
+      )
     }
   }
 }
