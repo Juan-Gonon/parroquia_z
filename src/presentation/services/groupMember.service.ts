cacd2232673d748fb781ca4f13a5df3a1eeb1fa1
Juan Gonon <juanfegonon27@gmail.com>
2025-10-16 19:59:19 -0600

    get members group add personal

--- src/presentation/services/groupMember.service.ts
+++ src/presentation/services/groupMember.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+/* eslint-disable @typescript-eslint/comma-dangle */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
@@ -9,61 +11,112 @@ import { UpdateMiembroGrupoDto } from '../../domain/DTOs/groupMember/UpdateGroup
 
 export class MiembroGrupoService {
   // Obtiene un miembro de grupo por su ID
-  public async getMiembroById (id: number): Promise<any> {
+  public async getMiembroById(id: number): Promise<any> {
     try {
-      const miembro = await prisma.miembrogrupo.findUnique({
-        where: { id_miembrogrupo: id }
+      const miembro = await prisma.miembrogrupo.findMany({
+        where: { id_gruposervicio: id },
+        select: {
+          id_miembrogrupo: true,
+          id_gruposervicio: true,
+          id_part_min: true,
+          fecha_ini_msia: true,
+          activo: true,
+          gruposervicio: {
+            select: {
+              nombre: true,
+            },
+          },
+          participacionministerio: {
+            select: {
+              fecha_ini_part: true,
+              personalparroquial: {
+                select: {
+                  nombre: true,
+                  apellido: true,
+                  email: true,
+                  direccion: true,
+                },
+              },
+              roldentroministerio: {
+                select: {
+                  nombre: true,
+                },
+              },
+            },
+          },
+        },
       })
       if (!miembro) {
         throw CustomError.badRequest('The requested member was not found.')
       }
       return miembro
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while retrieving the member.')
+      throw CustomError.internalServer(
+        'An error occurred while retrieving the member.'
+      )
     }
   }
 
   // Obtiene todos los miembros de grupo
-  public async getAllMiembros (pagination: PaginationDto) {
+  public async getAllMiembros(pagination: PaginationDto) {
     const { page, limit } = pagination
     try {
       const [total, miembros] = await prisma.$transaction([
         prisma.miembrogrupo.count(),
         prisma.miembrogrupo.findMany({
           skip: (page - 1) * limit,
-          take: limit
-        })
+          take: limit,
+        }),
       ])
       return {
         total,
         page,
         limit,
-        next: (page * limit) < total ? `/api/group-member?page=${page + 1}&limit=${limit}` : null,
-        prev: (page - 1 > 0) ? `/api/ministry?group-member=${page - 1}&limit=${limit}` : null,
-        data: miembros
+        next:
+          page * limit < total
+            ? `/api/group-member?page=${page + 1}&limit=${limit}`
+            : null,
+        prev:
+          page - 1 > 0
+            ? `/api/ministry?group-member=${page - 1}&limit=${limit}`
+            : null,
+        data: miembros,
       }
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while retrieving the members.')
+      throw CustomError.internalServer(
+        'An error occurred while retrieving the members.'
+      )
     }
   }
 
   // Crea un nuevo miembro de grupo
-  public async createMiembro (createDto: CreateMiembroGrupoDto): Promise<any> {
-    const { idPartMin, idGrupoServicio, fechaIni, roldentrogrupo, fechaFin, activo } = createDto
+  public async createMiembro(createDto: CreateMiembroGrupoDto): Promise<any> {
+    const {
+      idPartMin,
+      idGrupoServicio,
+      fechaIni,
+      roldentrogrupo,
+      fechaFin,
+      activo,
+    } = createDto
     try {
       // Validar que las entidades relacionadas existan
       const partMinExist = await prisma.participacionministerio.findUnique({
-        where: { id_part_min: idPartMin }
+        where: { id_part_min: idPartMin },
       })
       if (!partMinExist) {
-        throw CustomError.badRequest('The specified participation does not exist.')
+        throw CustomError.badRequest(
+          'The specified participation does not exist.'
+        )
       }
 
       const grupoServicioExist = await prisma.gruposervicio.findUnique({
-        where: { id_grupo: idGrupoServicio }
+        where: { id_grupo: idGrupoServicio },
       })
       if (!grupoServicioExist) {
-        throw CustomError.badRequest('The specified service group does not exist.')
+        throw CustomError.badRequest(
+          'The specified service group does not exist.'
+        )
       }
 
       const newMiembro = await prisma.miembrogrupo.create({
@@ -73,46 +126,56 @@ export class MiembroGrupoService {
           fecha_ini_msia: fechaIni,
           fecha_fin_msia: fechaFin ?? null,
           roldentrogrupo,
-          activo: activo !== undefined ? activo : true
-        }
+          activo: activo !== undefined ? activo : true,
+        },
       })
       return newMiembro
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while creating the member.')
+      throw CustomError.internalServer(
+        'An error occurred while creating the member.'
+      )
     }
   }
 
   // Actualiza un miembro de grupo existente
-  public async updateMiembro (updateDto: UpdateMiembroGrupoDto): Promise<any> {
+  public async updateMiembro(updateDto: UpdateMiembroGrupoDto): Promise<any> {
     const { id } = updateDto
     try {
       const miembroExist = await prisma.miembrogrupo.findUnique({
-        where: { id_miembrogrupo: id }
+        where: { id_miembrogrupo: id },
       })
       if (!miembroExist) {
-        throw CustomError.badRequest('The requested member to update was not found.')
+        throw CustomError.badRequest(
+          'The requested member to update was not found.'
+        )
       }
 
       // Validar que las entidades relacionadas existan si se van a actualizar
       if (updateDto.idPartMin) {
         const partMinExist = await prisma.participacionministerio.findUnique({
-          where: { id_part_min: updateDto.idPartMin }
+          where: { id_part_min: updateDto.idPartMin },
         })
         if (!partMinExist) {
-          throw CustomError.badRequest('The specified participation does not exist.')
+          throw CustomError.badRequest(
+            'The specified participation does not exist.'
+          )
         }
       }
       if (updateDto.idGrupoServicio) {
         const grupoServicioExist = await prisma.gruposervicio.findUnique({
-          where: { id_grupo: updateDto.idGrupoServicio }
+          where: { id_grupo: updateDto.idGrupoServicio },
         })
         if (!grupoServicioExist) {
-          throw CustomError.badRequest('The specified service group does not exist.')
+          throw CustomError.badRequest(
+            'The specified service group does not exist.'
+          )
         }
       }
 
       // Validar las fechas
-      const fechaIniP = updateDto.fechaIni ? updateDto.fechaIni : miembroExist.fecha_ini_msia
+      const fechaIniP = updateDto.fechaIni
+        ? updateDto.fechaIni
+        : miembroExist.fecha_ini_msia
       const fechaFinP = updateDto.fechaFin ? updateDto.fechaFin : null
       if (fechaFinP !== null && fechaIniP > fechaFinP) {
         throw CustomError.badRequest('fechaIni cannot be after fechaFin')
@@ -120,31 +183,37 @@ export class MiembroGrupoService {
 
       const updatedMiembro = await prisma.miembrogrupo.update({
         where: { id_miembrogrupo: id },
-        data: updateDto.values
+        data: updateDto.values,
       })
       return updatedMiembro
     } catch (error) {
       console.log(error)
-      throw CustomError.internalServer('An error occurred while updating the member.')
+      throw CustomError.internalServer(
+        'An error occurred while updating the member.'
+      )
     }
   }
 
   // Elimina un miembro de grupo por su ID
-  public async deleteMiembro (id: number) {
+  public async deleteMiembro(id: number) {
     try {
       const miembroExist = await prisma.miembrogrupo.findUnique({
-        where: { id_miembrogrupo: id }
+        where: { id_miembrogrupo: id },
       })
       if (!miembroExist) {
-        throw CustomError.badRequest('The requested member to delete was not found.')
+        throw CustomError.badRequest(
+          'The requested member to delete was not found.'
+        )
       }
 
       await prisma.miembrogrupo.delete({
-        where: { id_miembrogrupo: id }
+        where: { id_miembrogrupo: id },
       })
       return { message: 'Member successfully deleted.' }
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while deleting the member.')
+      throw CustomError.internalServer(
+        'An error occurred while deleting the member.'
+      )
     }
   }
 }
