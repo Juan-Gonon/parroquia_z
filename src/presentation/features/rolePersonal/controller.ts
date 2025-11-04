a8e0aab3eef57387a85009500dba4323fec779a9
Juan Gonon <juanfegonon27@gmail.com>
2025-11-04 11:21:43 -0600

    fix(build): Soluciona error TS2307 de módulo no encontrado en RolPersonal.service.ts

--- src/presentation/features/rolePersonal/controller.ts
+++ src/presentation/features/rolePersonal/controller.ts
@@ -1,16 +1,21 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable @typescript-eslint/space-before-function-paren */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { Request, Response } from 'express'
 import { CustomError, handleError, PaginationDto } from '../../../domain'
 import { CreateRolPersonalDto } from '../../../domain/DTOs/RolPersonal/CreateRolPersonal.dto'
-import { UpdateRolPersonalDto } from '../../../domain/DTOs/RolPersonal/updateRolPersonal.dto'
+import { UpdateRolPersonalDto } from '../../../domain/DTOs/RolPersonal/UpdateRolPersonal.dto'
 import { RolPersonalService } from '../../services/RolPersonal.service'
 
 export class RolPersonalController {
-  constructor (private readonly rolPersonalService: RolPersonalService) {}
+  constructor(private readonly rolPersonalService: RolPersonalService) {}
 
   // Obtener todos los roles de personal
-  public getAllRoles = async (req: Request, res: Response): Promise<Response> => {
+  public getAllRoles = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
     const { page = 1, limit = 10 } = req.query
 
     const [error, paginationDTO] = PaginationDto.create(+page, +limit)
@@ -25,10 +30,18 @@ export class RolPersonalController {
   }
 
   // Obtener un rol de personal por su ID
-  public getRolById = async (req: Request, res: Response): Promise<Response> => {
+  public getRolById = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
     const rolID = +req.params.id!
 
-    if (isNaN(rolID)) { return handleError(CustomError.badRequest('The id contains invalid characters'), res) }
+    if (isNaN(rolID)) {
+      return handleError(
+        CustomError.badRequest('The id contains invalid characters'),
+        res
+      )
+    }
 
     try {
       const rol = await this.rolPersonalService.getRolById(rolID)
@@ -53,7 +66,10 @@ export class RolPersonalController {
 
   // Actualizar un rol de personal
   public updateRol = async (req: Request, res: Response): Promise<Response> => {
-    const [error, updateDto] = UpdateRolPersonalDto.update({ ...req.body, id: req.params.id })
+    const [error, updateDto] = UpdateRolPersonalDto.update({
+      ...req.body,
+      id: req.params.id,
+    })
     if (error) return handleError(CustomError.badRequest(error), res)
 
     try {
@@ -68,7 +84,12 @@ export class RolPersonalController {
   public deleteRol = async (req: Request, res: Response): Promise<Response> => {
     const rolID = +req.params.id!
 
-    if (isNaN(rolID)) { return handleError(CustomError.badRequest('The id contains invalid characters'), res) }
+    if (isNaN(rolID)) {
+      return handleError(
+        CustomError.badRequest('The id contains invalid characters'),
+        res
+      )
+    }
 
     try {
       const response = await this.rolPersonalService.deleteRol(rolID)
--- src/presentation/services/RolPersonal.service.ts
+++ src/presentation/services/RolPersonal.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+/* eslint-disable @typescript-eslint/comma-dangle */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
@@ -5,21 +7,21 @@
 import { prisma } from '../../data/postgress'
 import { CustomError, PaginationDto } from '../../domain'
 import { CreateRolPersonalDto } from '../../domain/DTOs/RolPersonal/CreateRolPersonal.dto'
-import { UpdateRolPersonalDto } from '../../domain/DTOs/RolPersonal/updateRolPersonal.dto'
+import { UpdateRolPersonalDto } from '../../domain/DTOs/RolPersonal/UpdateRolPersonal.dto'
 
 export class RolPersonalService {
   // Obtener un RolPersonal por su nombre
-  public async getRolPersonalByName (nombre: string): Promise<any | null> {
+  public async getRolPersonalByName(nombre: string): Promise<any | null> {
     const rol = await prisma.rolpersonal.findFirst({
-      where: { nombre }
+      where: { nombre },
     })
     return rol
   }
 
   // Obtener un RolPersonal por su ID
-  public async getRolById (id: number): Promise<any> {
+  public async getRolById(id: number): Promise<any> {
     const rol = await prisma.rolpersonal.findUnique({
-      where: { id_rol: id }
+      where: { id_rol: id },
     })
     if (rol == null) {
       throw CustomError.badRequest('The requested RolPersonal was not found.')
@@ -28,9 +30,7 @@ export class RolPersonalService {
   }
 
   // Obtener todos los roles de personal con paginación
-  public async getAllRoles (
-    paginationDto: PaginationDto
-  ): Promise<any> {
+  public async getAllRoles(paginationDto: PaginationDto): Promise<any> {
     const { page, limit } = paginationDto
 
     try {
@@ -38,78 +38,94 @@ export class RolPersonalService {
         prisma.rolpersonal.count(),
         prisma.rolpersonal.findMany({
           skip: (page - 1) * limit,
-          take: limit
-        })
+          take: limit,
+        }),
       ])
 
       return {
         page,
         limit,
         total,
-        next: (page * limit) < total ? `/api/personal-role?page=${page + 1}&limit=${limit}` : null,
-        prev: (page - 1 > 0) ? `/api/personal-role?page=${page - 1}&limit=${limit}` : null,
-        data: roles
+        next:
+          page * limit < total
+            ? `/api/personal-role?page=${page + 1}&limit=${limit}`
+            : null,
+        prev:
+          page - 1 > 0
+            ? `/api/personal-role?page=${page - 1}&limit=${limit}`
+            : null,
+        data: roles,
       }
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while retrieving the data.')
+      throw CustomError.internalServer(
+        'An error occurred while retrieving the data.'
+      )
     }
   }
 
   // Crear un nuevo RolPersonal
-  public async createRol (
-    createRolDto: CreateRolPersonalDto
-  ): Promise<any> {
+  public async createRol(createRolDto: CreateRolPersonalDto): Promise<any> {
     const rolExists = await this.getRolPersonalByName(createRolDto.nombre)
     if (rolExists) {
-      throw CustomError.badRequest('A RolPersonal with that name already exists.')
+      throw CustomError.badRequest(
+        'A RolPersonal with that name already exists.'
+      )
     }
     try {
       const newRol = await prisma.rolpersonal.create({
         data: {
           nombre: createRolDto.nombre,
           descripcion: createRolDto.descripcion ?? null,
-          permisos: createRolDto.permisos ?? null
-        }
+          permisos: createRolDto.permisos ?? null,
+        },
       })
       return newRol
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while creating the RolPersonal.')
+      throw CustomError.internalServer(
+        'An error occurred while creating the RolPersonal.'
+      )
     }
   }
 
   // Actualizar un RolPersonal
-  public async updateRol (
-    updateRolDto: UpdateRolPersonalDto
-  ): Promise<any> {
+  public async updateRol(updateRolDto: UpdateRolPersonalDto): Promise<any> {
     const rolExists = await this.getRolById(updateRolDto.id)
     if (!rolExists) {
-      throw CustomError.badRequest('The requested RolPersonal to update was not found.')
+      throw CustomError.badRequest(
+        'The requested RolPersonal to update was not found.'
+      )
     }
 
     try {
       const updatedRol = await prisma.rolpersonal.update({
         where: { id_rol: updateRolDto.id },
-        data: { ...updateRolDto.values }
+        data: { ...updateRolDto.values },
       })
       return updatedRol
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while updating the RolPersonal.')
+      throw CustomError.internalServer(
+        'An error occurred while updating the RolPersonal.'
+      )
     }
   }
 
   // Eliminar un RolPersonal
-  public async deleteRol (id: number): Promise<any> {
+  public async deleteRol(id: number): Promise<any> {
     const rolExists = await this.getRolById(id)
     if (!rolExists) {
-      throw CustomError.badRequest('The requested RolPersonal to delete was not found.')
+      throw CustomError.badRequest(
+        'The requested RolPersonal to delete was not found.'
+      )
     }
     try {
       await prisma.rolpersonal.delete({
-        where: { id_rol: id }
+        where: { id_rol: id },
       })
       return { message: 'RolPersonal successfully deleted' }
     } catch (error) {
-      throw CustomError.internalServer('An error occurred while deleting the RolPersonal.')
+      throw CustomError.internalServer(
+        'An error occurred while deleting the RolPersonal.'
+      )
     }
   }
 }
