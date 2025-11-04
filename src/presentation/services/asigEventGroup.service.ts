736852e5c17350837be57b0b03ea452b1deac565
Juan Gonon <juanfegonon27@gmail.com>
2025-11-04 10:56:41 -0600

    asig event

--- src/presentation/services/asigEventGroup.service.ts
+++ src/presentation/services/asigEventGroup.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+/* eslint-disable @typescript-eslint/comma-dangle */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
@@ -8,7 +10,7 @@ import { UpdateAsigGrupoEventoDto } from '../../domain/DTOs/AssigEventGroup/Upda
 
 export class AsigGrupoEventoService {
   // Obtener todas las asignaciones
-  public async getAllAsignaciones (pagination: PaginationDto): Promise<any> {
+  public async getAllAsignaciones(pagination: PaginationDto): Promise<any> {
     const { page, limit } = pagination
 
     try {
@@ -16,20 +18,23 @@ export class AsigGrupoEventoService {
         prisma.asiggrupoevento.count(),
         prisma.asiggrupoevento.findMany({
           take: limit,
-          skip: (page - 1) * limit
-        })
+          skip: (page - 1) * limit,
+        }),
       ])
 
       return {
         page,
         limit,
         total,
-        next: (page * limit) < total ? `/api/asigeventgroup?page=${page + 1}&limit=${limit}` : null,
+        next:
+          page * limit < total
+            ? `/api/asigeventgroup?page=${page + 1}&limit=${limit}`
+            : null,
         prev:
-           (page - 1 > 0)
-             ? `/api/asigeventgroup?page=${page - 1}&limit=${limit}`
-             : null,
-        data: asignaciones
+          page - 1 > 0
+            ? `/api/asigeventgroup?page=${page - 1}&limit=${limit}`
+            : null,
+        data: asignaciones,
       }
     } catch (error) {
       throw CustomError.internalServer('Something went wrong')
@@ -37,11 +42,27 @@ export class AsigGrupoEventoService {
   }
 
   // Obtener una asignación por su ID
-  public async getAsignacionById (id: number): Promise<any> {
-    const asignacion = await prisma.asiggrupoevento.findUnique({
+  public async getAsignacionById(id: number): Promise<any> {
+    const asignacion = await prisma.asiggrupoevento.findMany({
       where: {
-        id_asig_ge: id
-      }
+        id_evento: id,
+      },
+      select: {
+        id_asig_ge: true,
+        id_evento: true,
+        id_grp_srv: true,
+        gruposervicio: {
+          select: {
+            id_grupo: true,
+            nombre: true,
+            ministerio: {
+              select: {
+                nombre: true,
+              },
+            },
+          },
+        },
+      },
     })
 
     if (!asignacion) {
@@ -52,33 +73,40 @@ export class AsigGrupoEventoService {
   }
 
   // Crear una nueva asignación
-  public async createAsignacion (createDto: CreateAsigGrupoEventoDto): Promise<any> {
+  public async createAsignacion(
+    createDto: CreateAsigGrupoEventoDto
+  ): Promise<any> {
     try {
       const nuevaAsignacion = await prisma.asiggrupoevento.create({
         data: {
           id_evento: createDto.idEvento,
           id_grp_srv: createDto.idGrpSrv,
-          notas: createDto.notas ?? null
-        }
+          notas: createDto.notas ?? null,
+        },
       })
 
       return nuevaAsignacion
     } catch (error: any) {
-      if (error.code === 'P2003') { // Foreign key constraint failed
+      if (error.code === 'P2003') {
+        // Foreign key constraint failed
         throw CustomError.badRequest('Referenced id not found')
       }
-      throw CustomError.internalServer('Something went wrong creating the asignacion')
+      throw CustomError.internalServer(
+        'Something went wrong creating the asignacion'
+      )
     }
   }
 
   // Actualizar una asignación
-  public async updateAsignacion (updateDto: UpdateAsigGrupoEventoDto): Promise<any> {
+  public async updateAsignacion(
+    updateDto: UpdateAsigGrupoEventoDto
+  ): Promise<any> {
     const { id } = updateDto
 
     const asignacionExistente = await prisma.asiggrupoevento.findUnique({
       where: {
-        id_asig_ge: id
-      }
+        id_asig_ge: id,
+      },
     })
 
     if (!asignacionExistente) {
@@ -88,27 +116,30 @@ export class AsigGrupoEventoService {
     try {
       const asignacionActualizada = await prisma.asiggrupoevento.update({
         where: {
-          id_asig_ge: id
+          id_asig_ge: id,
         },
-        data: updateDto.values
+        data: updateDto.values,
       })
 
       return asignacionActualizada
     } catch (error: any) {
-      if (error.code === 'P2003') { // Foreign key constraint failed
+      if (error.code === 'P2003') {
+        // Foreign key constraint failed
         throw CustomError.badRequest('Referenced id not found')
       }
       console.log(error)
-      throw CustomError.internalServer('Something went wrong updating the asignacion')
+      throw CustomError.internalServer(
+        'Something went wrong updating the asignacion'
+      )
     }
   }
 
   // Eliminar una asignación
-  public async deleteAsignacion (id: number): Promise<any> {
+  public async deleteAsignacion(id: number): Promise<any> {
     const asignacionExistente = await prisma.asiggrupoevento.findUnique({
       where: {
-        id_asig_ge: id
-      }
+        id_asig_ge: id,
+      },
     })
 
     if (!asignacionExistente) {
@@ -118,12 +149,14 @@ export class AsigGrupoEventoService {
     try {
       await prisma.asiggrupoevento.delete({
         where: {
-          id_asig_ge: id
-        }
+          id_asig_ge: id,
+        },
       })
       return { message: `Asignacion with id ${id} deleted` }
     } catch (error) {
-      throw CustomError.internalServer('Something went wrong deleting the asignacion')
+      throw CustomError.internalServer(
+        'Something went wrong deleting the asignacion'
+      )
     }
   }
 }
